f891594abe054632310c21ca7aaddbf74059abec
Sean Stangl <sean.stangl@gmail.com>
2019-07-30 10:03:48 -0600

    Upload OpenLifter v1.0 (pending general availability tomorrow).

---
+++ releases/1.0/precache-manifest.97e54282c38150cf1e3ded912c370a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1573441308edd6bf7dc9",
+    "url": "/releases/1.0/static/js/runtime~main.15734413.js"
+  },
+  {
+    "revision": "b7a3f2da0ba723ac428f",
+    "url": "/releases/1.0/static/js/main.b7a3f2da.chunk.js"
+  },
+  {
+    "revision": "8dd21c960e28bad68fc0",
+    "url": "/releases/1.0/static/js/1.8dd21c96.chunk.js"
+  },
+  {
+    "revision": "b7a3f2da0ba723ac428f",
+    "url": "/releases/1.0/static/css/main.176ca901.chunk.css"
+  },
+  {
+    "revision": "8dd21c960e28bad68fc0",
+    "url": "/releases/1.0/static/css/1.305568d2.chunk.css"
+  },
+  {
+    "revision": "70d1192a27e8fe0fea65b3213848f86a",
+    "url": "/releases/1.0/index.html"
+  }
+];
---
+++ releases/1.0/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/releases/1.0/precache-manifest.97e54282c38150cf1e3ded912c370a66.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/releases/1.0/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
