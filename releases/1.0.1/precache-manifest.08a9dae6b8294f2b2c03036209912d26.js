3e2c65c8a4e2b70089e620e5ea9dfdda89fd0c53
Sean Stangl <sean.stangl@gmail.com>
2019-08-05 11:33:58 -0600

    Release OpenLifter 1.0.1

---
+++ releases/1.0.1/precache-manifest.08a9dae6b8294f2b2c03036209912d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbfb4e27686b8430ce0f",
+    "url": "/releases/1.0.1/static/js/runtime~main.cbfb4e27.js"
+  },
+  {
+    "revision": "265b3c9d6f1adcda88da",
+    "url": "/releases/1.0.1/static/js/main.265b3c9d.chunk.js"
+  },
+  {
+    "revision": "8dd21c960e28bad68fc0",
+    "url": "/releases/1.0.1/static/js/1.8dd21c96.chunk.js"
+  },
+  {
+    "revision": "265b3c9d6f1adcda88da",
+    "url": "/releases/1.0.1/static/css/main.176ca901.chunk.css"
+  },
+  {
+    "revision": "8dd21c960e28bad68fc0",
+    "url": "/releases/1.0.1/static/css/1.305568d2.chunk.css"
+  },
+  {
+    "revision": "5bf334720e015d79f7a6235daf2dc37c",
+    "url": "/releases/1.0.1/index.html"
+  }
+];
