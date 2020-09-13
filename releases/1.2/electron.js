d4c1c902dc0db83f92efc8b1b799049ac6a28e48
Sean Stangl <sean.stangl@gmail.com>
2020-09-13 15:40:23 -0600

    Release OpenLifter 1.2

---
+++ releases/1.2/electron.js
@@ -0,0 +1,47 @@
+// vim: set ts=2 sts=2 sw=2 et:
+//
+// This file is part of OpenLifter, simple Powerlifting meet software.
+// Copyright (C) 2019 The OpenPowerlifting Project.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ releases/1.2/precache-manifest.3a7562ca9bfc6d87707bb970e0648ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89d057e3bf12df611624",
+    "url": "/releases/1.2/static/css/main.27baa6ad.chunk.css"
+  },
+  {
+    "revision": "89d057e3bf12df611624",
+    "url": "/releases/1.2/static/js/main.89d057e3.chunk.js"
+  },
+  {
+    "revision": "8b031a882dbe6b12b6cb",
+    "url": "/releases/1.2/static/css/1.b032c8b0.chunk.css"
+  },
+  {
+    "revision": "8b031a882dbe6b12b6cb",
+    "url": "/releases/1.2/static/js/1.8b031a88.chunk.js"
+  },
+  {
+    "revision": "c3339f25b9a9b8735fec",
+    "url": "/releases/1.2/static/js/runtime~main.c3339f25.js"
+  },
+  {
+    "revision": "e8b59c69e56aae15adc6b73af54c3734",
+    "url": "/releases/1.2/index.html"
+  }
+];
