7d6c5c794a3d543a8714affaaa2c63ee240c1a97
Sean Stangl <sean.stangl@gmail.com>
2021-01-07 11:06:16 -0700

    Release OpenLifter 1.3

---
+++ releases/1.3/precache-manifest.30f590127cddb33f142dcd5195ba4d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdedef9155ad5c4b0af9",
+    "url": "/releases/1.3/static/css/main.758c3168.chunk.css"
+  },
+  {
+    "revision": "fdedef9155ad5c4b0af9",
+    "url": "/releases/1.3/static/js/main.fdedef91.chunk.js"
+  },
+  {
+    "revision": "b0224f4cea1407bcae4b",
+    "url": "/releases/1.3/static/css/1.613932cc.chunk.css"
+  },
+  {
+    "revision": "b0224f4cea1407bcae4b",
+    "url": "/releases/1.3/static/js/1.b0224f4c.chunk.js"
+  },
+  {
+    "revision": "647af17cbbe49ce2986f",
+    "url": "/releases/1.3/static/js/runtime~main.647af17c.js"
+  },
+  {
+    "revision": "34fd22ca4aa69c0a84f331321bece2a1",
+    "url": "/releases/1.3/index.html"
+  }
+];
